5997128e47ff25c3e2006385fdb3aca7205c104b
Johan <1938159279@qq.com>
2024-12-28 15:54:28 +0800

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return 'Hello World';
+import { User } from "@/types";
+
+export default async function Home() {
+  const users = (await fetch("https://johan-server.onrender.com").then((res) =>
+    res.json()
+  )) as User[];
+  return (
+    <>
+      {users.map(({ id, name, age, email }) => (
+        <div key={id}>
+          <div>name: {name}</div>
+          <div>age: {age}</div>
+          <div>email: {email}</div>
+        </div>
+      ))}
+    </>
+  );
 }
