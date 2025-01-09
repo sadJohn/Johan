81c5060dd50bbf59d30674bbd42cbeda0085d5f9
Johan <1938159279@qq.com>
2025-01-09 22:04:42 +0800

    fonts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -6,8 +6,12 @@ import {
 } from "next/font/google";
 
 export const geistFont = Geist({
+  subsets: ["latin"],
   fallback: ["Arial", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"],
 });
-export const audiowideFont = Audiowide({ weight: "400" });
+export const audiowideFont = Audiowide({ weight: "400", subsets: ["latin"] });
 export const playwriteFont = Playwrite_AU_SA();
-export const rubikBubblesFont = Rubik_Bubbles({ weight: "400" });
+export const rubikBubblesFont = Rubik_Bubbles({
+  weight: "400",
+  subsets: ["latin"],
+});
