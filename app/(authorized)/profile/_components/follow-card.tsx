bc99c3e423b6ef11213a825ab30ef2be2bb8548e
王天一 <1938159279@qq.com>
2025-01-22 17:03:26 +0800

    🦄 refactor: follow card

--- app/(authorized)/profile/_components/follow-card.tsx
+++ app/(authorized)/profile/_components/follow-card.tsx
@@ -2,7 +2,9 @@ import ActionButton from "@/components/common/action-button";
 import UserAvatar from "@/components/profile/user-avatar";
 import {
   Card,
+  CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
@@ -16,23 +18,19 @@ interface FollowCardProps {
 
 const FollowCard = ({ user, action, label }: FollowCardProps) => {
   return (
-    <Card>
-      <CardHeader className="flex flex-row justify-between">
-        <div className="flex flex-row items-end gap-2">
-          <UserAvatar user={user} />
-          <div>
-            <CardTitle>{user.username}</CardTitle>
-            <CardDescription>
-              Deploy your new project in one-click.
-            </CardDescription>
-          </div>
-        </div>
-        <div>
-          <form action={action}>
-            <ActionButton>{label}</ActionButton>
-          </form>
-        </div>
+    <Card className="flex flex-row">
+      <CardHeader>
+        <UserAvatar user={user} />
       </CardHeader>
+      <CardContent className="flex-1 self-end p-6 pl-0">
+        <CardTitle>{user.username}</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardContent>
+      <CardFooter className="p-6">
+        <form action={action}>
+          <ActionButton>{label}</ActionButton>
+        </form>
+      </CardFooter>
     </Card>
   );
 };
